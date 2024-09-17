72c5d3356c55c93da2b392c9323829c31f282f15
Miitto <wpbickerton@gmail.com>
2024-09-17 13:31:05 +0100

    Add Tauri

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,7 @@
-mod grid;
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use grid::Grid;
+mod grid;
 
 fn main() {
     #[rustfmt::skip]
@@ -32,4 +33,9 @@ fn main() {
         elapsed.subsec_micros(),
         elapsed.subsec_nanos()
     );
+
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
